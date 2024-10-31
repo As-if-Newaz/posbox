bda0700f07a975323b2ffed419a6dffdc2a529dc
Asif Newaz <asif.kravmaga@gmail.com>
2024-10-31 20:10:18 +0600

    First CRUD interface IRepo added

---
+++ DALWHOLEPOS/Interfaces/IRepo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DALWHOLEPOS.Interfaces
+{
+    public interface IRepo<CLASS,ID,RETURN>
+    {
+        RETURN Create(CLASS obj);
+        List<CLASS> Get();
+
+        CLASS Get(ID id);
+
+        RETURN Update(CLASS obj);
+
+        bool Delete(ID id);
+    }
+}
